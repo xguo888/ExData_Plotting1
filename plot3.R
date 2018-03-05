87fcfa0e0c781c0275b5dbaea9ea56ccfd15a0d8
xguo888 <31167782+xguo888@users.noreply.github.com>
2018-03-04 22:05:08 -0700

    plot3 script

---
+++ plot3.R
@@ -0,0 +1,12 @@
+with(mydata, {plot(Sub_metering_1~Datetime, type="l", ylab="Global Active Power (kilowatts)", xlab="" )
+  lines(Sub_metering_2~Datetime, col="Red")
+  lines(Sub_metering_3~Datetime, col="Blue")
+  })
+legend("topright", col=c("black", "red", "blue"), lwd=c(1,1,1), c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+
+#save file
+dev.copy(png, file="plot3.png", height=480, width=480)
+
+#close device
+dev.off()
